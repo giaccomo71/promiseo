db03321e4492c97b9879f3f5f9a1e54b98c2c2b5
michalruml <michal.ruml@inveo.cz>
2022-04-28 23:01:19 +0200

    updated files

--- js/script.js
+++ js/script.js
@@ -27,16 +27,135 @@ btnHamburger.addEventListener("click", function () {
 });
 
 // Slider
-(function($) {
-  var cover = $('#hero'),
-      covers = $('.fadecovers');
+(function ($) {
+  var cover = $("#hero"),
+    covers = $(".fadecovers");
 
-  covers.children(':not(:last-child)').hide();
+  /* covers.children(":not(:last-child)").hide();
 
-  setInterval(function() {
-    covers.children(':last-child')
-      .fadeOut(1500, function() { $(this).prependTo( covers ); })
-      .prev().fadeIn(1500);
-  }, 2500);
+  var sliderInterval = setInterval(function () {
+    covers
+      .children(":last-child")
+      .fadeOut(1500, function () {
+        $(this).prependTo(covers);
+      })
+      .prev()
+      .fadeIn(1500);
+  }, 5000); */
 
+  var Slider = {
+    intervalID: null,
+    running: false,
+    set: function (id) {
+      this.intervalID = id;
+    },
+    get: function () {
+      return "IDcko intervalu je: " + this.intervalID;
+    },
+    start: function () {
+      this.intervalID = setInterval(function () {
+        covers
+          .children(":last-child")
+          .fadeOut(1500, function () {
+            $(this).prependTo(covers);
+          })
+          .prev()
+          .fadeIn(1500);
+      }, 5000);
+      this.running = true;
+    },
+    pause: function () {
+      clearInterval(this.intervalID);
+      this.intervalID = null;
+      this.running = false;
+    },
+    resume: function () {
+      if (!this.intervalID) this.start();
+    },
+    toggle: function () {
+      if (this.running) this.pause();
+      else this.resume();
+    },
+  };
+
+  Slider.start();
+
+  covers.on("click", function () {
+    Slider.toggle();
+  });
+
+  // DISPLAY REFERENCE
+  var points = $("#references__points");
+  var copy = $(".references__copy");
+  var a =
+    "Nunc risus massa, sollicitudin eu purus ac, congue pulvinar quam. Duis id enim at risus vestibulum convallis.";
+  var b = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+  var c = "Mauris sapien nibh, rhoncus at blandit non, imperdiet nec nibh.";
+  var d = "Pellentesque efficitur nulla a justo varius ultrices.";
+  var e = "Praesent hendrerit diam nec elementum interdum.";
+  var f = "Quisque pellentesque sapien id tellus lacinia efficitur.";
+  var g =
+    "Fusce eget leo elit. Praesent fermentum risus magna, vitae vulputate elit lacinia in.";
+
+  copy.text(a);
+
+  points.find("button:nth-child(1)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(a);
+
+    event.preventDefault();
+  });
+
+  points.find("button:nth-child(2)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(b);
+
+    event.preventDefault();
+  });
+
+  points.find("button:nth-child(3)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(c);
+
+    event.preventDefault();
+  });
+
+  points.find("button:nth-child(4)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(d);
+
+    event.preventDefault();
+  });
+
+  points.find("button:nth-child(5)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(e);
+
+    event.preventDefault();
+  });
+
+  points.find("button:nth-child(6)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(f);
+
+    event.preventDefault();
+  });
+
+  points.find("button:nth-child(7)").on("click", function (event) {
+    //copy.text($(this).text());
+    copy.text(g);
+
+    event.preventDefault();
+  });
 })(jQuery);
+
+// Add active class to the current point (highlight it)
+var highpoint = document.getElementById("references__points");
+var btns = highpoint.getElementsByClassName("references__point");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
